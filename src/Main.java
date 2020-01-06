db9d500be4d7472d73c32d822d5bc48407a6b566
Ilya Klishin <klishin.it@gmail.com>
2020-01-06 12:57:04 +0300

    Added hello, world separately

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        System.out.println("a > b = " + (a > b));
-        System.out.println("a < b = " + (a < b));
-        System.out.println("a = b ? " + (a == b));
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+        String helloWithSpace = "Hello ";
+        System.out.println(helloWithSpace + world + "!");
     }
 }

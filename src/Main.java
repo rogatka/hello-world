98cb94825b5c3fb299324b59313503ab657795e3
Ilya Klishin <klishin.it@gmail.com>
2020-01-06 12:53:10 +0300

    Added a,b comparison

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int a = 1;
         int b = 2;
-        int c = a + b;
-        System.out.println("sum = " + c);
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a < b = " + (a < b));
+        System.out.println("a = b ? " + (a == b));
     }
 }
